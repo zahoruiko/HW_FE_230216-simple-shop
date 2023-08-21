2749d3b386b25242289cdf250fa8a0482b12e98a
Oleksii Zahoruiko <zahoruiko.oleksii@gmail.com>
2023-08-21 17:24:04 +0200

    Updates

---
+++ precache-manifest.32bd44b4f2fc7f81e16d7805dab6504a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c2d02b8501e82027e5",
+    "url": "/HW_FE_230216-simple-shop/static/css/main.afb80742.chunk.css"
+  },
+  {
+    "revision": "11c2d02b8501e82027e5",
+    "url": "/HW_FE_230216-simple-shop/static/js/main.11c2d02b.chunk.js"
+  },
+  {
+    "revision": "71d1f42601588a34bb98",
+    "url": "/HW_FE_230216-simple-shop/static/js/1.71d1f426.chunk.js"
+  },
+  {
+    "revision": "217873854a5e064839f4",
+    "url": "/HW_FE_230216-simple-shop/static/js/runtime~main.21787385.js"
+  },
+  {
+    "revision": "b8c89d1576ec38d31b7067a4a583c29a",
+    "url": "/HW_FE_230216-simple-shop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HW_FE_230216-shop/precache-manifest.1886e688a51af44710c42c09d473caf9.js"
+  "/HW_FE_230216-simple-shop/precache-manifest.32bd44b4f2fc7f81e16d7805dab6504a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/HW_FE_230216-shop/index.html", {
+workbox.routing.registerNavigationRoute("/HW_FE_230216-simple-shop/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
